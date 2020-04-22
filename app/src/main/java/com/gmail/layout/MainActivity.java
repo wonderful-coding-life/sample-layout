15b61bec45b2e15c5be57b3c3e8fcc846d941fde
spiderman <spiderman@xhift.co.kr>
2020-04-22 10:29:06 +0900

    added constraint layout sample

--- app/src/main/java/com/gmail/layout/MainActivity.java
+++ app/src/main/java/com/gmail/layout/MainActivity.java
@@ -2,7 +2,9 @@ package com.gmail.layout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showLinearLayoutSample(View view) {
+        Intent intent = new Intent(this, LinearActivity.class);
+        startActivity(intent);
+    }
+
+    public void showConstraintLayoutSample(View view) {
+        Intent intent = new Intent(this, ConstraintActivity.class);
+        startActivity(intent);
     }
 }
